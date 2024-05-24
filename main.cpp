a390984714da3bb83133df14ebf9b8fb9c2a20f4
postmodernist1488 <elitic.pantheism@gmail.com>
2024-05-24 21:44:03 +0300

    Add Container concept test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "bucket_storage.hpp"
 #include "iostream"
+#include <type_traits>
 
 #include <gtest/gtest.h>
 
+#include <concepts>
 #include <random>
+#include <utility>
 
 #define LOGGING 0
 void p()
@@ -203,6 +206,58 @@ TEST(bucket_storage, random)
 			check();
 		}
 	}
+}
+
+template< class ContainerType >
+concept Container = requires(ContainerType a, const ContainerType b) {
+	requires std::regular< ContainerType >;
+	requires std::swappable< ContainerType >;
+	requires std::destructible< typename ContainerType::value_type >;
+	requires std::same_as< typename ContainerType::reference, typename ContainerType::value_type & >;
+	requires std::same_as< typename ContainerType::const_reference, const typename ContainerType::value_type & >;
+	requires std::forward_iterator< typename ContainerType::iterator >;
+	requires std::forward_iterator< typename ContainerType::const_iterator >;
+	requires std::signed_integral< typename ContainerType::difference_type >;
+	requires std::same_as< typename ContainerType::difference_type, typename std::iterator_traits< typename ContainerType::iterator >::difference_type >;
+	requires std::same_as< typename ContainerType::difference_type, typename std::iterator_traits< typename ContainerType::const_iterator >::difference_type >;
+	{
+		a.begin()
+	} -> std::same_as< typename ContainerType::iterator >;
+	{
+		a.end()
+	} -> std::same_as< typename ContainerType::iterator >;
+	{
+		b.begin()
+	} -> std::same_as< typename ContainerType::const_iterator >;
+	{
+		b.end()
+	} -> std::same_as< typename ContainerType::const_iterator >;
+	{
+		a.cbegin()
+	} -> std::same_as< typename ContainerType::const_iterator >;
+	{
+		a.cend()
+	} -> std::same_as< typename ContainerType::const_iterator >;
+	{
+		a.size()
+	} -> std::same_as< typename ContainerType::size_type >;
+	{
+		a.max_size()
+	} -> std::same_as< typename ContainerType::size_type >;
+	{
+		a.empty()
+	} -> std::same_as< bool >;
+};
+
+template< Container C >
+void container_f(C c)
+{
+}
+
+TEST(bucket_storage, container)
+{
+	EXPECT_TRUE(Container< BucketStorage< S > >);
+	// container_f(BucketStorage< S >()); // uncomment to see why it's failing
 }
 
 int main(int argc, char **argv)
